09ff9534b2cce6b464325601193b9cc441ea20af
WhiteHairHAN <protoss1207@gmail.com>
2022-03-06 01:34:30 +0900

    Upload initial files

---
+++ js/tetris.js
@@ -0,0 +1,9 @@
+const blockArea = document.querySelector(".blockArea > ul");
+
+for(let i; i<20;i++){
+    const ul = document.createElement("ul");
+    for(let j=0;j<10;j++){
+        const li = document.createElement("li");
+        blockArea.appendChild("li");
+    }
+}
